a3ca4b508821f2e95b1a75f6986d103a3f40417b
Jun <chromejun97@gmail.com>
2021-09-18 23:59:47 +0800

    - Additional script to PlayerSystem for ease of retrieving value

---
+++ SEAGamejam2021 - Kingpin/Assets/Scripts/Pin/PlayerHandCollider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandCollider : MonoBehaviour
+{
+    [SerializeField] LaunchBalls launchBalls = null;
+
+    public bool IsAttacking { get { return launchBalls.isAttacking; } }
+}
