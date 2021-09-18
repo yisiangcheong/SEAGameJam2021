d39c36dd961004cb93cdb223bfeed3717c8d52f0
Nicholas Tung <nick.tungks@gmail.com>
2021-09-18 14:23:36 +0800

    - basic charge mechanic added to ball launcher

--- SEAGamejam2021 - Kingpin/Assets/_Kingpin/LaunchBalls.cs
+++ SEAGamejam2021 - Kingpin/Assets/_Kingpin/LaunchBalls.cs
@@ -6,25 +6,45 @@ public class LaunchBalls : MonoBehaviour
 {
     [SerializeField] Rigidbody leftBall = null;
     [SerializeField] Rigidbody rightBall = null;
+    [SerializeField] float minForce = 1f;
+    [SerializeField] float maxForce = 10f;
+    [SerializeField] float forceChargeRate = 1f;
     [SerializeField] float forceMultiplier = 1f;
+    float currentForce = 0f;
     [SerializeField] Transform cameraFocus = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ResetForce();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        print(currentForce);
+        if (Input.GetMouseButton(0))
         {
-            LaunchBall(leftBall);
+            ChargeForce();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            LaunchBall(leftBall, currentForce);
+            ResetForce();
         }
     }
 
-    void LaunchBall(Rigidbody ball)
+    void LaunchBall(Rigidbody ball, float force)
     {
-        ball.AddForce(cameraFocus.forward * forceMultiplier, ForceMode.Impulse);
+        ball.AddForce(cameraFocus.forward * force * forceMultiplier, ForceMode.Impulse);
+    }
+
+    void ChargeForce()
+    {
+        currentForce += forceChargeRate * Time.deltaTime;
+    }
+
+    void ResetForce()
+    {
+        currentForce = minForce;
     }
 }
