5b045b640454867ef3fb4974155feaecee2fa586
Nicholas Tung <nick.tungks@gmail.com>
2021-09-18 16:28:54 +0800

    - ground pound layer setup - max force for punch implemented

---
+++ SEAGamejam2021 - Kingpin/Assets/GroundPound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPound : MonoBehaviour
+{
+    [SerializeField] float minVelocityForPound = 10f;
+    [SerializeField] Rigidbody playerRigidbody = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground") && playerRigidbody.velocity.magnitude >= minVelocityForPound)
+        {
+            print("Hit the ground");
+        }
+    }
+}
--- SEAGamejam2021 - Kingpin/Assets/_Kingpin/LaunchBalls.cs
+++ SEAGamejam2021 - Kingpin/Assets/_Kingpin/LaunchBalls.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LaunchBalls : MonoBehaviour
 {
+    [SerializeField] Transform player = null;
+    [SerializeField] Transform cameraFocus = null;
     [SerializeField] Rigidbody leftBall = null;
     [SerializeField] Rigidbody rightBall = null;
     [SerializeField] float minForce = 1f;
@@ -11,7 +13,14 @@ public class LaunchBalls : MonoBehaviour
     [SerializeField] float forceChargeRate = 1f;
     [SerializeField] float forceMultiplier = 1f;
     float currentForce = 0f;
-    [SerializeField] Transform cameraFocus = null;
+    [SerializeField] float ballMaxDistance = 5f;
+    [SerializeField] bool rightHandMode = false;
+    [SerializeField] float kinematicFreezeDuration = 1.0f;
+    [SerializeField] bool leftBallWasKinematic = true;
+    [SerializeField] bool rightBallWasKinematic = true;
+    [SerializeField] Vector3 leftBallPosition = Vector3.zero;
+    [SerializeField] Vector3 rightBallPosition = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,16 +30,30 @@ public class LaunchBalls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(currentForce);
         if (Input.GetMouseButton(0))
         {
             ChargeForce();
         }
         if (Input.GetMouseButtonUp(0))
         {
-            LaunchBall(leftBall, currentForce);
+            if (rightHandMode)
+            {
+                LaunchBall(rightBall, currentForce);
+                rightHandMode = false;
+                rightBallWasKinematic = false;
+            }
+            else
+            {
+                LaunchBall(leftBall, currentForce);
+                rightHandMode = true;
+                leftBallWasKinematic = false;
+            }
             ResetForce();
         }
+        //SetKinematic();
+        //SetBallMaxDistance();
+        //leftBallPosition = leftBall.transform.position;
+        //rightBallPosition = rightBall.transform.position;
     }
 
     void LaunchBall(Rigidbody ball, float force)
@@ -40,11 +63,50 @@ public class LaunchBalls : MonoBehaviour
 
     void ChargeForce()
     {
-        currentForce += forceChargeRate * Time.deltaTime;
+        if (currentForce < maxForce) currentForce += forceChargeRate * Time.deltaTime;
     }
 
     void ResetForce()
     {
         currentForce = minForce;
+    }
+
+    void SetKinematic()
+    {
+        if(!leftBallWasKinematic)
+        {
+            if (Vector3.Distance(player.position, leftBallPosition) > Vector3.Distance(player.position, leftBall.transform.position))
+            {
+                leftBall.transform.position = leftBallPosition;
+                StartCoroutine(SetKinematicRoutine(leftBall));
+                leftBallWasKinematic = true;
+            }
+        }
+        if(!rightBallWasKinematic)
+        {
+            if (Vector3.Distance(player.position, rightBallPosition) > Vector3.Distance(player.position, rightBall.transform.position))
+            {
+                rightBall.transform.position = rightBallPosition;
+                StartCoroutine(SetKinematicRoutine(rightBall));
+                rightBallWasKinematic = true;
+            }
+        }
+    }
+
+    IEnumerator SetKinematicRoutine(Rigidbody ball)
+    {
+        ball.isKinematic = true;
+        yield return new WaitForSeconds(kinematicFreezeDuration);
+        ball.isKinematic = false;
+        yield break;
+    }
+
+    void SetBallMaxDistance()
+    {
+        if(!rightHandMode)
+        {
+            if (Vector3.Distance(player.position, rightBall.transform.position) > ballMaxDistance) rightBall.transform.position = rightBallPosition;
+        }
+        else if (Vector3.Distance(player.position, leftBall.transform.position) > ballMaxDistance) leftBall.transform.position = leftBallPosition;
     }
 }
