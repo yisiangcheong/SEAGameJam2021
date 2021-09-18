5354cd65f8fc604616841a7e11e0a00890bf9c78
Cheong Yi Siang <yisiang_98@hotmail.com>
2021-09-19 02:13:50 +0800

    AudioManager added to MainScene

---
+++ SEAGamejam2021 - Kingpin/Assets/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Player SFX Events")]
+    [FMODUnity.EventRef]
+    public string PlayerHurtEvent = "";
+
+    [FMODUnity.EventRef]
+    public string PlayerPunchEvent = "";
+
+    [Header("Pin SFX Events")]
+    [FMODUnity.EventRef]
+    public string PinAttackEvent = "";
+
+    [FMODUnity.EventRef]
+    public string GiantPinAttackEvent = "";
+
+    [FMODUnity.EventRef]
+    public string PinHurtEvent = "";
+
+    [FMODUnity.EventRef]
+    public string PinBowlingStrikeEvent = "";
+
+    [Header("Dialogue/Voice SFX Events")]
+    [FMODUnity.EventRef]
+    public string AnikiEvent = "";
+
+    [FMODUnity.EventRef]
+    public string SussyBakaEvent = "";
+
+    public void PlaySFX(string targetEvent, GameObject targetGO)
+    {
+        FMODUnity.RuntimeManager.PlayOneShotAttached(targetEvent, targetGO);
+    }
+}
