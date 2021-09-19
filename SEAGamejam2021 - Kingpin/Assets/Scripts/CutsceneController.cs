0aed5f6fbdc8cc198419881e049aec08bcd73929
Jun <chromejun97@gmail.com>
2021-09-19 12:21:02 +0800

    CutsceneController improvements

--- SEAGamejam2021 - Kingpin/Assets/Scripts/CutsceneController.cs
+++ SEAGamejam2021 - Kingpin/Assets/Scripts/CutsceneController.cs
@@ -11,24 +11,72 @@ public class CutsceneController : MonoBehaviour
     [SerializeField] Image fade = null;
     [SerializeField] RawImage videoDisplay = null;
     [SerializeField] VideoPlayer videoPlayer = null;
+    [SerializeField] Text skipLabel = null;
 
     [Header("Settings")]
     [SerializeField] float playbackDelay = 0.5f;
     [SerializeField] float fadeOutDelay = 1f;
     [SerializeField] float fadeDuration = 1.0f;
     [SerializeField] string sceneToLoad = "";
+    [SerializeField] float skipConfirmation = 2.5f;
 
     Color transparent = new Color(0.0f, 0.0f, 0.0f, 0.0f);
     Color fullAlpha = new Color(0.0f, 0.0f, 0.0f, 1.0f);
 
     Color fullWhite = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 
+    Coroutine checkSkipCutsceneRoutine = null;
     Coroutine splashScreenRoutine = null;
+
+    public bool isTransitioning { get; private set; } = false;
 
     private void OnEnable()
     {
+        skipLabel.color = transparent;
+
+        if (checkSkipCutsceneRoutine != null) StopCoroutine(checkSkipCutsceneRoutine);
+        checkSkipCutsceneRoutine = StartCoroutine(CheckSkipCutsceneRoutine());
+
         if (splashScreenRoutine != null) StopCoroutine(splashScreenRoutine);
         splashScreenRoutine = StartCoroutine(SplashscreenRoutine());
+    }
+
+    void TransitionToNextScene()
+    {
+        if (isTransitioning) return;
+        isTransitioning = true;
+
+        StopAllCoroutines();
+        StartCoroutine(TransitionToNextSceneRoutine());
+    }
+
+    IEnumerator CheckSkipCutsceneRoutine()
+    {
+        float timer = 0.0f;
+
+        while (true)
+        {
+            if (timer <= 0.0f)
+            {
+                skipLabel.color = transparent;
+
+                if (Input.anyKeyDown)
+                {
+                    timer = skipConfirmation;
+                    skipLabel.color = fullWhite;
+                }
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+
+                if (Input.anyKeyDown) break;
+            }
+
+            yield return null;
+        }
+
+        TransitionToNextScene();
     }
 
     IEnumerator SplashscreenRoutine()
@@ -43,6 +91,11 @@ public class CutsceneController : MonoBehaviour
 
         yield return new WaitForSeconds(fadeOutDelay);
 
+        TransitionToNextScene();
+    }
+
+    IEnumerator TransitionToNextSceneRoutine()
+    {
         float timer = 0.0f;
 
         while (timer < fadeDuration)
